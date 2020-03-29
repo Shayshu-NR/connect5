2636444bdb10fe1fd6b3b57b998e36b4e81e2afe
Shayshu Nahata <55864616+Shayshu-NR@users.noreply.github.com>
2020-03-29 17:31:03 -0400

    Update connect5.c

--- connect5.c
+++ connect5.c
@@ -1,1 +1,32 @@
 #include <stdlib.h>
+
+/*~~~~~Function Prototypes */
+char** board_set_up(int x, int y);
+/*~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+int main(void){
+    char** board = board_set_up(7, 8);
+    
+    return 0;
+}
+
+
+
+//Sets up the intial board data structure and sets
+//each entry to empty ('e')
+char** board_set_up(int x, int y){
+    char** initial_board = malloc(x * sizeof(char*));
+
+    for(int i = 0; i < x; i++){
+        initial_board[i] = malloc(y * sizeof(char*));
+    }
+
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < y; j++){
+            initial_board[i][j] = 'e';
+        }
+    }
+
+    return initial_board
+}
